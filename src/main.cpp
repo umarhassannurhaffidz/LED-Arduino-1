1019fa1f101056fdf9fddcde7f85c1479a4babe3
“umar <umarhassannurhaffidzsalmun@gmail.com>
2025-02-11 11:54:00 +0700

    update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Deklarasi pin LED 
+// Mendeklarasikan variabel konstanta integer untuk menimpan namor LED 
+const int ledMerah = 8;
+const int ledKuning = 9;
+const int ledHijau = 10;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+
+  //Mengatur pin-pin yang. di hubungkan ke LED output.
+  //Hal ini berarti pin-pin tersebut dapat di gunakan untuk mengendalikan LED.
+  pinMode(ledMerah, OUTPUT);
+  pinMode(ledKuning, OUTPUT);
+  pinMode(ledHijau, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+  //led Merah
+  //menyalakan led Merah selama 2 detik, kemudian mematikannya.
+  digitalWrite(ledMerah, HIGH);
+  delay(2000);
+  digitalWrite(ledMerah, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ //led Kuning 
+ //menyalakan led Merah selama 2 detik, kemudian mematikannya.
+ digitalWrite(ledKuning, HIGH);
+ delay(2000);
+ digitalWrite(ledKuning, LOW);
+
+ //led Hijau
+ //menyalakan led Hijau selama 2 detik, kemudian mematikannya.
+ digitalWrite(ledHijau, OUTPUT);
+ delay(2000);
+ digitalWrite(ledHijau, LOW);
+
 }
